e61d9bc10abb1a5f8828e59a4434be8a2b261a5a
Kilomebit17 <oleh.stahnyuk@gmail.com>
2021-08-15 16:56:45 +0300

    fix bug with email required

--- src/app/components/auth/login-form/login-form.component.ts
+++ src/app/components/auth/login-form/login-form.component.ts
@@ -1,42 +1,42 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, Validators, FormGroup, FormControl} from "@angular/forms";
-import {AuthService} from "../auth.service";
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  Validators,
+  FormGroup,
+  FormControl,
+} from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss']
+  styleUrls: ['./login-form.component.scss'],
 })
 export class LoginFormComponent implements OnInit {
   form: FormGroup;
   loading = false;
   submitted = false;
 
-  constructor(
-    private formBuilder: FormBuilder,
-    public auth:AuthService
-  ) {
-  }
-  loginWithGoogle() {
-    this.auth.googleSignIn()
-  }
+  constructor(private formBuilder: FormBuilder, public auth: AuthService) {}
+  loginWithGoogle() {}
 
   ngOnInit(): void {
     this.form = new FormGroup({
       email: new FormControl(null, Validators.required),
-      password: new FormControl(null, [Validators.required, Validators.minLength(6)])
-    })
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
   }
-
 
   onSubmit() {
     this.submitted = true;
 
     const userLogin = {
-      email:this.form.value.email,
-      password:this.form.value.password
-    }
-    this.auth.signIn(userLogin)
+      email: this.form.value.email,
+      password: this.form.value.password,
+    };
+    this.auth.signIn(userLogin);
   }
-
 }
--- src/app/components/auth/register-form/register-form.component.ts
+++ src/app/components/auth/register-form/register-form.component.ts
@@ -1,42 +1,47 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
-import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
-import {AuthService} from "../auth.service";
-import {Router} from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
-  styleUrls: ['./register-form.component.scss']
+  styleUrls: ['./register-form.component.scss'],
 })
 export class RegisterFormComponent implements OnInit {
   form: FormGroup;
   loading = false;
   submitted = false;
-  errorMess: string
+  errorMess: string;
 
-  constructor(
-    public auth: AuthService) {
-  }
+  constructor(public auth: AuthService) {}
 
   ngOnInit(): void {
     this.form = new FormGroup({
-      email: new FormControl(null, Validators.email),
-      password: new FormControl(null, [Validators.required, Validators.minLength(6)])
-    })
+      email: new FormControl('', Validators.required),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
   }
 
-
-  onSubmit() { //* submit form
+  onSubmit() {
+    //* submit form
     this.submitted = true;
 
     this.loading = true;
 
-    this.auth.signUp(this.form.value)
+    this.auth.signUp(this.form.value);
   }
 
   loginWithGoogle() {
-    this.auth.googleSignIn()
+    this.auth.googleSignIn();
   }
-
 }
